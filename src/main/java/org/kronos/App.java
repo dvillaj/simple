0399ecdf37473e1c595a8c6e84f3e53d27d92ab2
Daniel Villanueva <dvillaj@gmail.com>
2019-11-07 05:58:08 +0100

    Initial commit

---
+++ src/main/java/org/kronos/App.java
@@ -0,0 +1,13 @@
+package org.kronos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
